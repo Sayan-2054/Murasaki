68728736d73bb683909de3ecaa656bd270d6eb3f
Sayantan Chowdhury <135856824+Sayan-2054@users.noreply.github.com>
2025-04-21 23:43:34 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import MurasakiApp from './components/MurasakiApp'
+import "./App.css"
+
+const App = () => {
+  return (
+    <div>
+      <MurasakiApp />
+    </div>
+  )
+}
+
+export default App
